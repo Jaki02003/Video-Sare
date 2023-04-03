8789994410c3e6b549b750c53e34bd4a61e6fdeb
MEEM <MEEM@DESKTOP-VDUJR2L>
2023-04-03 15:26:55 +0600

    Add project files.

---
+++ Video Sare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Video_Sare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
